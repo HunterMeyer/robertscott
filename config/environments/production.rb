e35559e2dbd0fe53636db3f3054b76c9cf1fb3af
HunterMeyer <huntermeyer@live.com>
2015-10-08 22:30:55 -0400

    Configure app settings and css compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ RobertScott::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -64,7 +64,7 @@ RobertScott::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'robertscott' }
+  config.action_mailer.default_url_options = { host: 'robertscott.xyz' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
