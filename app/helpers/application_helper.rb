1611b6f92ab918d8a078371f2d72e049b146629e
HunterMeyer <huntermeyer@live.com>
2015-10-08 23:03:46 -0400

    Format materialize pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = 'RobertScott'
+    if page_title.empty?
+      base_title + ' | Entertaining Short Videos'
+    else
+      page_title + ' | ' + base_title
+    end
+  end
+
+  def markdown(text)
+    html = Redcarpet::Render::HTML.new(prettify: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(html, space_after_headers: true, fenced_code_blocks: true)
+    markdown.render(text).html_safe
+  end
+
+  def generate_form_errors(errors)
+    content_tag(:div, class: 'error') do
+      'Oops. Something went wrong:'.html_safe +
+      content_tag(:ul) do
+        errors.full_messages.map { |message| content_tag(:li, message.gsub(/(Last|First)/, 'Full')) }.uniq.join.html_safe
+      end
+    end
+  end
 end
