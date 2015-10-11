b63afefa846ccc3092737968f4e2abb4f47aaad6
HunterMeyer <huntermeyer@live.com>
2015-10-11 18:06:09 -0400

    Add video modal

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,4 +2,22 @@ $(document).ready(function() {
   $('.button-collapse').sideNav();
   $('.close-mobie-sidebar').click(function() { $('.button-collapse').sideNav('hide'); }); 
   $('.alert-dismissible .close').click(function() { $(this).parent().fadeOut(); })
+  
+  video_player = $('#video-player');
+  video_link = '';
+  $('.modal-trigger').click(function() { video_link = $(this); });
+  $('.modal-trigger').leanModal({
+    dismissible: true,
+    opacity: .5,
+    in_duration: 300,
+    out_duration: 200,
+    ready: function() {
+      video_id  = video_link.data('video-id');
+      embed_url = 'https://www.youtube.com/embed/' + video_id + '?rel=0&autoplay=1';
+      external_url = 'https://www.youtube.com/watch?v=' + video_id;
+      video_player.attr('src', embed_url);
+      video_player.parent().parent().prev().children('#modal-yt-link').attr('href', external_url);
+    },
+    complete: function() { video_player.attr('src', ''); }
+  });
 });
